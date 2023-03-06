1d5e88f005f438ef185f018c891d5795c74ec83a
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-06 10:34:32 -0700

    archivos nuevos creados

---
+++ includes/head.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/boxicons.min.css" />
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="assets/css/meanmenu.css" />
+    <link rel="stylesheet" href="assets/css/fancybox.min.css" />
+    <link rel="stylesheet" href="assets/css/odometer.min.css" />
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="assets/css/scrollCue.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/dark.css" />
+    <link rel="stylesheet" href="assets/css/responsive.css" />
+    <title>Ruzo - Restaurant & Cafe HTML Template</title>
+    <link rel="icon" type="image/webp" href="assets/images/logo-2.webp" />
+</head>
---
+++ includes/header.php
@@ -0,0 +1,290 @@
+<div class="submenu-area submenu-two-area">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-lg-4 col-md-3">
+                <div class="submenu-right-content">
+                    <ul>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <i class="bx bxl-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/" target="_blank">
+                                <i class="bx bxl-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank">
+                                <i class="bx bxl-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank">
+                                <i class="bx bxl-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-9">
+                <div class="submenu-left-content">
+                    <ul>
+                        <li>
+                            <img src="assets/images/phone-icon.svg" alt="icon" />
+                            <a href="tel:(+0188)76870859">(+0188) 7687 0859</a>
+                        </li>
+                        <li>
+                            <img src="assets/images/email-icon.svg" alt="icon" />
+                            <a
+                                href="https://templates.hibootstrap.com/cdn-cgi/l/email-protection#ea828f868685aa989f90859e828f878f99c4898587"><span
+                                    class="__cf_email__"
+                                    data-cfemail="ddb5b8b1b1b29dafa8a7b2a9b5b8b0b8aef3beb2b0">[email&#160;protected]</span></a>
+                        </li>
+                        <li>
+                            <img src="assets/images/map.svg" alt="icon" />
+                            <p>4b, Walse Street , USA</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="navbar-area navbar-two-area">
+    <div class="container">
+        <div class="mobile-nav">
+            <div class="logo">
+                <a href="index.html">
+                    <img src="assets/images/logo-1.webp" class="logo-light" alt="images" />
+                    <img src="assets/images/logo-2.webp" class="logo-dark" alt="images" />
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-nav main-two-nav">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item nav-item-five">
+                            <a href="#" class="nav-link dropdown-toggle active">
+                                Home
+                                <i class="bx bx-plus"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="index.html" class="nav-link active">Home One</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="index-2.html" class="nav-link">Home Two</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="index-3.html" class="nav-link">Home Three</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item nav-item-five">
+                            <a href="#" class="nav-link dropdown-toggle">
+                                Pages
+                                <i class="bx bx-plus"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="about.html" class="nav-link">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="booking-table.html" class="nav-link">Booking Table</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="team.html" class="nav-link">Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="testimonials.html" class="nav-link">Testimonials</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="faq.html" class="nav-link">FAQ’s</a>
+                                </li>
+                                <li class="nav-item nav-item-five">
+                                    <a href="#" class="nav-link dropdown-toggle">
+                                        Portfolio
+                                        <i class="bx bx-plus"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="portfolio.html" class="nav-link">Portfolio</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="portfolio-details.html" class="nav-link">Portfolio Details</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link dropdown-toggle">
+                                        User
+                                        <i class="bx bx-plus"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="my-account.html" class="nav-link bg-border-top">My Account</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="login.html" class="nav-link">Login</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="register.html" class="nav-link">Register</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="forgot-password.html" class="nav-link bg-border-2">Forgot
+                                                Password</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="terms-conditions.html" class="nav-link">Terms & Conditions</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="error.html" class="nav-link">Error</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="coming-soon.html" class="nav-link bg-border-2">Coming Soon</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle">
+                                Menu
+                                <i class="bx bx-plus"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="menu.html" class="nav-link">Menu</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="menu-details.html" class="nav-link">Menu Details</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item nav-item-five">
+                            <a href="#" class="nav-link dropdown-toggle">
+                                Shop
+                                <i class="bx bx-plus"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="shop.html" class="nav-link">Shop</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="shop-details.html" class="nav-link">Shop Details</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="cart.html" class="nav-link">Cart</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="checkout.html" class="nav-link">Checkout</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item nav-item-five">
+                            <a href="#" class="nav-link dropdown-toggle">
+                                Blog
+                                <i class="bx bx-plus"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="blog.html" class="nav-link bg-border-top">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="blog-details.html" class="nav-link bg-border-2">Blog Details</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contact.html" class="nav-link">Contact Us</a>
+                        </li>
+                        <li class="nav-item logo">
+                            <a href="index.html" class="nav-link">
+                                <img src="assets/images/logo-1.webp" class="logo-light" alt="images" />
+                                <img src="assets/images/logo-2.webp" class="logo-dark" alt="images" />
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="others-option-vg d-flex align-items-center">
+                        <div class="option-item">
+                            <i class="bx bx-search search-btn"></i>
+                            <i class="bx bx-x close-btn"></i>
+                            <div class="search-overlay search-popup">
+                                <div class="search-box">
+                                    <form class="search-form">
+                                        <input class="search-input" placeholder="Search..." type="text" />
+                                        <button class="search-button" type="submit">
+                                            <i class="bx bx-search"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="option-item">
+                            <div class="shapping-bag">
+                                <a href="cart.html">
+                                    <img src="assets/images/shopping-bag-icon.svg" alt="images" />
+                                </a>
+                                <div class="shapping-text">01</div>
+                            </div>
+                        </div>
+                        <div class="option-item">
+                            <a href="booking-table.html" class="default-btn">Book A Table</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div class="others-option-for-responsive">
+        <div class="container">
+            <div class="dot-menu">
+                <div class="inner">
+                    <div class="circle circle-one"></div>
+                    <div class="circle circle-two"></div>
+                    <div class="circle circle-three"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="option-inner">
+                    <div class="others-option justify-content-center d-flex align-items-center">
+                        <div class="option-item">
+                            <i class="bx bx-search search-btn"></i>
+                            <i class="bx bx-x close-btn"></i>
+                            <div class="search-overlay search-popup">
+                                <div class="search-box">
+                                    <form class="search-form">
+                                        <input class="search-input" placeholder="Search..." type="text" />
+                                        <button class="search-button" type="submit">
+                                            <i class="bx bx-search"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="option-item">
+                            <div class="shapping-bag">
+                                <a href="cart.html">
+                                    <img src="assets/images/shopping-bag-icon.svg" alt="images" />
+                                </a>
+                                <div class="shapping-text">01</div>
+                            </div>
+                        </div>
+                        <div class="option-item">
+                            <a href="booking-table.html" class="default-btn">Book A Table</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
