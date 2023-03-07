727a11fee323960bd86abc3f8b294b61efc777e8
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-07 16:07:37 -0700

    contacto ok

---
+++ modules/contact.php
@@ -0,0 +1,121 @@
+<div class="pag-banner-area contact-pages-area jarallax" data-jarallax='{"speed": 0.3}'>
+    <div class="container">
+        <div class="page-banner-content">
+            <h1>Contáctanos</h1>
+            <ul>
+                <li><a style="color:#000;" href="/">Inicio</a></li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="contact-from-area pt-100 pb-100">
+    <div class="container">
+        <div class="contact-form-box">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="single-contact-form">
+                        <div class="section-title left-title">
+                            <h2>Reserva con nosotros</h2>
+                        </div>
+                        <div class="contact-from">
+                            <form id="contactForm">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="name" id="name" class="form-control"
+                                                placeholder="Nombre*" required data-error="Please enter your name" />
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" name="phone_number" id="phone_number"
+                                                placeholder="Correo*" required data-error="Please enter your number"
+                                                class="form-control" />
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="form-group">
+                                            <input type="text" name="email" id="email" class="form-control"
+                                                placeholder="Teléfono*" required data-error="Please enter your email" />
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="form-group">
+                                            <input type="text" name="subject" id="subject" class="form-control"
+                                                placeholder="Asunto" required data-error="Please enter your Subject" />
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <textarea name="message" class="form-control" id="message" cols="30"
+                                                rows="5" placeholder="Mensaje*" required
+                                                data-error="Write your message"></textarea>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 col-md-12">
+                                        <button type="submit" class="default-btn">
+                                            Enviar
+                                        </button>
+                                        <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="contact-form-content">
+                        <div class="section-title left-title">
+                            <span class="top-title">Contáctanos</span>
+                            <h2>Datos de contacto</h2>
+                            <p>
+
+                                Ubicada en el corazón de San José del Cabo, con nuestro diverso
+                                menú campesino ofrece desde una comida formal hasta el arte de
+                                la pizza artesanal napolitana.
+
+                            </p>
+                        </div>
+                        <div class="contact-widget">
+                            <div class="contact-item">
+                                <i class="bx bxs-phone-call"></i>
+                                <h3>Teléfono:</h3>
+                                <a href="tel:6241308444">(624) 130 8444</a>
+                            </div>
+                            <div class="contact-item">
+                                <i class="bx bx-envelope"></i>
+                                <h3>Correo:</h3>
+                                <a target="_blank" href="mailto:reservations@laramona.com.mx"><span>
+                                        reservations@laramona.com.mx</span></a>
+                            </div>
+                            <div class="contact-item">
+                                <i class="bx bx-map"></i>
+                                <h3>Dirección:</h3>
+                                <a target="_blank" href="https://goo.gl/maps/SnUVg8gY19Win9LF6">
+                                    <p>Gallery District, Centro</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contact-map-area">
+    <div class="container-fluid">
+        <div class="google-map">
+            <iframe class="maps"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d522.5539753789794!2d-109.69589345002055!3d23.062434173356323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af514278a5fed7%3A0xd9565185df7b0624!2sLa%20Ramona%20Restaurant!5e0!3m2!1ses-419!2smx!4v1678229492086!5m2!1ses-419!2smx"></iframe>
+        </div>
+    </div>
+</div>
